d20770da5af0908f81a50879e69dcd6ee626ce2a
vivianifederico <viviani.federico.maria@gmail.com>
2016-07-23 22:50:00 +0200

    new commit

---
+++ BasicCalculator/src/basiccalculator/Operations.java
@@ -0,0 +1,32 @@
+
+package basiccalculator;
+
+public class Operations {
+    double firstNumber;
+    double secondNumber;
+
+    public Operations(double firstNumber, double secondNumber) {
+        this.firstNumber = firstNumber;
+        this.secondNumber = secondNumber;
+    }
+
+    public double moltiplicazione() {
+        return this.firstNumber * this.secondNumber;
+    }
+
+    public double divisione() {
+        if (this.secondNumber == 0.0) {
+            System.out.println("Errore, valore non corretto");
+            return 0.0;
+        }
+        return this.firstNumber / this.secondNumber;
+    }
+
+    public double somma() {
+        return this.firstNumber + this.secondNumber;
+    }
+
+    public double differenza() {
+        return this.firstNumber - this.secondNumber;
+    }
+}
